62d5e7e8d630f3ca81b79352d3f860a0f2d0676a
Chris <chrisflanagan23@gmail.com>
2020-07-18 14:28:14 -0700

    Add Rest Module - closes #6

---
+++ application/src/main/java/retail/RetailApplication.java
@@ -0,0 +1,11 @@
+package retail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class RetailApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RetailApplication.class, args);
+    }
+}
